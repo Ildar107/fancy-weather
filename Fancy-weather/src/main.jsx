55ff701fcee15bb39b2af7f4432e379518473662
Ildar107 <ildar107wk@gmail.com>
2020-05-31 21:15:23 +0300

    init: project fancy-weather

---
+++ Fancy-weather/src/main.jsx
@@ -0,0 +1,10 @@
+//import 'bootswatch/dist/journal/bootstrap.min.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/styles/style.css';
+
+
+ReactDOM.render(
+  <span>ПОка не готово!</span>,
+  document.getElementById('root'),
+);
